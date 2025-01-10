d5856016400c2f7ed980f3fe6525a45c1f309026
Carson Payne <carson.j.payne14@gmail.com>
2025-01-10 10:07:01 -0600

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Carson Payne on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
