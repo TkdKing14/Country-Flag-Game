9f72c9118b58ea7210d8a092d439dfe839dfc418
Carson Payne <carson.j.payne14@gmail.com>
2025-01-10 21:55:21 -0600

    Created Data and Game Manager

---
+++ Models/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Carson Payne on 1/10/25.
+//
+
+import Foundation
---
+++ Models/GameManager.swift
@@ -0,0 +1,8 @@
+//
+//  GameManager.swift
+//  Country Flag Game
+//
+//  Created by Carson Payne on 1/10/25.
+//
+
+import Foundation
